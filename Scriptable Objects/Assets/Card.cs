dd58441a2915236b9bc4c039cbdb68b133a81db5
dampo4 <33032031+dampo4@users.noreply.github.com>
2021-05-19 15:07:09 +0100

    scriptable objects demo

---
+++ Scriptable Objects/Assets/Card.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+	public new string name;
+	public string description;
+	public string type;
+
+	public Sprite artwork;
+
+	public int starLevel;
+	public int attack;
+	public int defence;
+
+}
---
+++ Scriptable Objects/Assets/CardDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+	public Text type;
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+	public List<Image> starLevels;
+	public Text attackText;
+	public Text defenceText;
+	private int currStarLevel;
+    // Use this for initialization
+    private void Start()
+    {
+		currStarLevel = card.starLevel;
+    }
+    void Update () {
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+		type.text = "[ " + card.type + " ]";
+		attackText.text = "ATK/" + card.attack.ToString();
+		defenceText.text = "DEF/" + card.defence.ToString();
+	
+        if (currStarLevel == card.starLevel)
+        {
+			for (int i = 0; i < card.starLevel; i++)
+			{
+				starLevels[i].gameObject.SetActive(true);
+			}
+		}
+        else
+        {
+			for (int i = 0; i < 5; i++)
+			{
+				starLevels[i].gameObject.SetActive(false);
+			}
+		}
+		currStarLevel = card.starLevel;
+	}
+	
+}
